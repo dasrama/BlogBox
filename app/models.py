45a8f8b715fbf88091a48c6460fc1f2dfdda0aec
dasrama <rama.idmail@gmail.com>
2024-03-31 22:34:40 +0530

    added two new fields i.e published and created_at

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,Boolean
 from .database import Base
 import datetime
-
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class Post(Base):
     __tablename__ = "posts"
@@ -9,6 +10,8 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='true', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
     
 
 
