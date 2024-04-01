17ad952e14d072686e2c095be9d280f6d0669fde
dasrama <rama.idmail@gmail.com>
2024-04-01 18:29:20 +0530

    experimenting with pydantic for producing correct response body

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
-
+from datetime import datetime
 
 class Post(BaseModel):
     title: str
     content: str
-    #published: bool = True
+    published: bool = True
+    # created_at : datetime
+
+    """class Config:
+        orm_mode = True"""
+
