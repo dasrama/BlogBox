b03961b04d54917a8c8cee03d580e4c9397dd34c
dasrama <rama.idmail@gmail.com>
2024-04-01 18:33:25 +0530

    checking the response type of each request sent

--- app/main.py
+++ app/main.py
@@ -8,9 +8,11 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
+    # print(type(posts))
     return posts
 
 
@@ -20,18 +22,18 @@ def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-
+    # print(type(posts))
     return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}",response_model = schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id)
 
     if post.first()==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
-
+    # print(type(post))
     return post.first()
 
 
