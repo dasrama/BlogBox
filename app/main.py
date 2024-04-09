598c3e429b7441ddc946e4528e3abc48ad76e868
dasrama <rama.idmail@gmail.com>
2024-04-10 01:25:07 +0530

    created user route for handling new user

--- app/main.py
+++ app/main.py
@@ -73,3 +73,32 @@ def update_post(id: int, update_post: schemas.Post, db: Session = Depends(get_db
     db.commit()
 
     return {"msg":"successfully updated the post"}
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.CreateUserResponse)
+def create_user(user: schemas.CreateUserRequest, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    #print(user.email)
+    query = db.query(models.User.email).filter(models.User.email == user.email)
+    #print(query)
+    existing_user = query.first()
+    if existing_user != None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="email already registered")
+    
+    # **user.dict() unpacks the dictionary user into keyword arguments and pass it into the fields of User model defined in models.py
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    #print(type(new_user))
+    return new_user
+
+
+
+"""
+from pydantic import ValidationError
+
+try:
+    ...
+    
+except ValidationError as e:
+    raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e))"""
