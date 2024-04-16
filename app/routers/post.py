2998ceac4484d20fc0a5551f870b871f46b1b82e
dasrama <rama.idmail@gmail.com>
2024-04-16 16:56:54 +0530

    cleaned up some unused imports

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,8 +1,8 @@
 
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from .. import models, schemas,utils
-from ..database import get_db,engine
+from .. import models, schemas
+from ..database import get_db
 
 router = APIRouter()
 
