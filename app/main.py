ebd3e7a21a0f77c6876e33fc8c34410e9a62b9df
dasrama <rama.idmail@gmail.com>
2024-04-16 17:06:16 +0530

    Updated import statements to reflect new file structure for user-related API endpoints.

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas,utils
 from .database import get_db,engine
 import psycopg2
-from .routers import post
+from .routers import post, user
 
 
 
@@ -14,31 +14,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(post.router)
+app.include_router(user.router)
 
 
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.CreateUserResponse)
-def create_user(user: schemas.CreateUserRequest, db: Session = Depends(get_db)):
-    # print(user.password)
-    # hash the password - user.password
-    hashedPassword = utils.hash(user.password)
-    user.password = hashedPassword
-
-    new_user = models.User(**user.dict())
-    #print(user.email)
-    query = db.query(models.User.email).filter(models.User.email == user.email)
-    #print(query)
-    existing_user = query.first()
-    if existing_user != None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="email already registered")
-    
-    # **user.dict() unpacks the dictionary user into keyword arguments and pass it into the fields of User model defined in models.py
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    #print(type(new_user))
-    return new_user
 
 
 
