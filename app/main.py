d534a565d6c145aad23eaf517983b54b29a49a36
dasrama <rama.idmail@gmail.com>
2024-05-22 23:42:05 +0530

    included authentication router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+import psycopg2
+
 from . import models, schemas,utils
 from .database import get_db,engine
-import psycopg2
-from .routers import post, user
-
-
+from .routers import post, user, auth
 
 
 # will create all the corresponding tables in the database based on models defined in model.py .
@@ -15,6 +13,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
+
 
 
 
