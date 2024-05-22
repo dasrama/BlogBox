2f2a7825b18258aee9182a7454d44ad8fb1e4c9e
dasrama <rama.idmail@gmail.com>
2024-05-22 23:39:02 +0530

    implemented high level logic of user login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,25 @@
-from fastapi import Depends, APIRouter, Response
-from .. import database
+from fastapi import Depends, APIRouter, Response, HTTPException, status
+from .. import database, schemas
 from sqlalchemy.orm import Session
+from .. import models, utils
 
-router = APIRouter()
+
+router = APIRouter(tags=['Authentication'])
+
 
 @router.post("/login")
-def login(db : Session= Depends(database.get_db)):
-    ...
+def login(user_credentials : schemas.UserLogin, db : Session= Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+     
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "invalid user credentials")
+    
+    # print(user)
+    
+    if not utils.verify(plain_password= user_credentials.password,hashed_password= user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials")
+
+    # create a token
+    # return token
+    return {"token" : "example"}
+    
